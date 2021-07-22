0552c6148ccd9a5c4c69724abb8b8a7716d8667f
pradeepvarun <pradeep.varun@yandex.com>
2021-07-23 03:19:19 +0530

    I have added class to this learning python.

--- one.py
+++ one.py
@@ -8,13 +8,9 @@ class emp:
         self.pay=pay
     def apply_raise(self):
         self.pay= float(self.pay * self.raise_amount)
-
-
-
-emp1=emp("santu","var",20)
+emp1=emp("prad","var",20)
 emp2=emp("santu","var",20)
 emp1.raise_amount=10
-
 print (emp.apply_raise(emp1))
 print (emp.apply_raise(emp2))
 print((emp1.pay))
